675537440687d721d312b57e4470629df9d9a45f
noobpersonTOs <46160387+noobpersonTOs@users.noreply.github.com>
2019-02-03 21:44:38 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -152,7 +152,7 @@ async def stats():
 	
 @bot.command()
 async def invite():
-	embed = discord.Embed(description=" ", color=0x800080)
+	embed = discord.Embed(description=" ", color=0xFFFF)
 	embed.add_field(name="Bot invite", value=f"[Bot invite](https://discordapp.com/api/oauth2/authorize?client_id=409253229491126285&permissions=469854214&redirect_uri=https%3A%2F%2Fdiscordapp.com%2Fapi%2Foauth2%2Fauthorize%3Fclient_id%3D409253229491126285%26permissions%3D2146958839%26scope%3Dbot&scope=bot)")
 	embed.add_field(name="Support server", value=f"[https://discord.gg/bweznkF](https://discord.gg/bweznkF)")
 	await bot.say(embed=embed)
