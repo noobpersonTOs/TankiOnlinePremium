42cb5c7d1bf8795375ae80af34d101431752be41
noobpersonTOs <46160387+noobpersonTOs@users.noreply.github.com>
2019-02-12 21:59:55 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -167,26 +167,26 @@ async def ratings(ctx, user: str):
             try:
                 response = (await r.json ())["response"]
                 await bot.send_typing(ctx.message.channel)
-                kills = response["kills"]
-                deaths = response["deaths"]
-                crystals = response["earnedCrystals"]
-                gold = response["caughtGolds"]
-                experience = response["score"]
-                premium = response["hasPremium"]
-		rank = response["RankName"]
-                embed = discord.Embed(title="Statistics for {}".format(user), url="http://ratings.tankionline.com/en/user/{}/".format(user), \
-                                    descrption="Tanki Online", color=0x42d9f4)
+		kills = response["kills"]
+		deaths = response["deaths"]
+		crystals = response["earnedCrystals"]
+		gold = response["caughtGolds"]
+		experience = response["score"]
+		premium = response["hasPremium"]
+		level = response["Rank"]
+		embed = discord.Embed(title="Statistics for {}".format(user), url="http://ratings.tankionline.com/en/user/{}/".format(user), \
+				      descrption="Tanki Online", color=0x42d9f4)
 		embed.add_field(name="Rank", value="{:}".format(rank))
-                embed.add_field(name="Premium Account", value="{}".format(premium))
-                embed.add_field(name="Experience", value="{:,}".format(experience))
-                embed.add_field(name="Crystals Obtained", value="{:,}".format(crystals))
-                embed.add_field(name="Gold Boxes Caught", value="{:,}".format(gold))
-                embed.add_field(name="Kills", value="{:,}".format(kills))
-                embed.add_field(name="Deaths", value="{:,}".format(deaths))
-                embed.add_field(name="KD", value="{0:.2f}".format(kills/deaths))
-                await bot.say(embed=embed)
-            except:
-                await bot.say("Account does not exist.")
+		embed.add_field(name="Premium Account", value="{}".format(premium))
+		embed.add_field(name="Experience", value="{:,}".format(experience))
+		embed.add_field(name="Crystals Obtained", value="{:,}".format(crystals))
+		embed.add_field(name="Gold Boxes Caught", value="{:,}".format(gold))
+		embed.add_field(name="Kills", value="{:,}".format(kills))
+		embed.add_field(name="Deaths", value="{:,}".format(deaths))
+		embed.add_field(name="KD", value="{0:.2f}".format(kills/deaths))
+		await bot.say(embed=embed)
+	except:
+		await bot.say("Account does not exist.")
     			
 @bot.command(pass_context=True)
 async def noobs(ctx, *, reportmsg: str):
