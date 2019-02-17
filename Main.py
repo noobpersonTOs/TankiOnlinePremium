28753e26e95361edd0a3b95825a072e6c760d8e9
noobpersonTOs <46160387+noobpersonTOs@users.noreply.github.com>
2019-02-16 18:21:08 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -231,13 +231,11 @@ cont = ["**|*** You found a common item: **3,500 Crystals**",
 @bot.event
 async def on_message(message):
 	if message.content.startswith("d!c open"):
-		author = message.author
 		channel = message.channel
 		await bot.send_typing(message.channel)
 		embed = discord.Embed(title="Tanki Online", url="https://discordbots.org/bot/409253229491126285", descrption="Tanki Online", color=0x42d9f4)
 		embed.set_thumbnail(url="https://imgur.com/yf0oeDe.png")
 		embed.add_field(name="container", value=random.choice(cont))
-		embed.set_footer(text=" | {}".format(author), icon_url="https://imgur.com/yf0oeDe.png")
 		await bot.send_message(channel, embed=embed)
 	await bot.process_commands(message)
 	
