f87541e4cb5cba30293bbab02d0d8e67f855663b
noobpersonTOs <46160387+noobpersonTOs@users.noreply.github.com>
2019-02-16 17:27:57 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -390,19 +390,19 @@ async def ratings(ctx, user: str):
 				gold = response["caughtGolds"]
 				experience = response["score"]
 				premium = response["hasPremium"]
-				rank = response["rank"]
+				ranks = response["rank"]
 				gearscore = response["gearScore"]
-				supplies = response["suppliesUsed"]
+				supply = response["supplies"]
 				embed = discord.Embed(title="Statistics for {}".format(user), url="http://ratings.tankionline.com/en/user/{}/".format(user), \
 						      description="Profile:", color=0x42d9f4)
 				embed.add_field(name="Nickname", value="{}".format(user), inline=False)
-				embed.add_field(name="Rank", value="{}".format(rank), inline=False)
+				embed.add_field(name="Rank", value="{}".format(ranks), inline=False)
 				embed.add_field(name="Premium Account", value="{}".format(premium), inline=False)
 				embed.add_field(name="Experience", value="{:,}".format(experience), inline=False)
 				embed.add_field(name="Crystals Obtained", value="{:,}".format(crystals), inline=False)
 				embed.add_field(name="Gold Boxes Caught", value="{:,}".format(gold), inline=False)
 				embed.add_field(name="Gear Score", value="{}".format(gearscore), inline=False)
-				embed.add_field(name="Supplies Used", value="{:,}".format(supplies), inline=False)
+				embed.add_field(name="Supplies Used", value="{:,}".format(supply), inline=False)
 				embed.add_field(name="Kills", value="{:,}".format(kills), inline=False)
 				embed.add_field(name="Deaths", value="{:,}".format(deaths), inline=False)
 				embed.add_field(name="K/D", value="{0:.2f}".format(kills/deaths), inline=False)
